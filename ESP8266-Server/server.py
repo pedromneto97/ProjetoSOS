ae297be9db32d922e9f7d46b64f114ada0b11e23
pedromneto97 <pedromneto97@gmail.com>
2018-04-19 19:01:43 -0300

    Server

---
+++ ESP8266-Server/server.py
@@ -0,0 +1,37 @@
+import network
+import socket
+
+
+class Server:
+    # station = network.WLAN(network.STA_IF)
+    ssid = "Pedro"
+    password = "12345678"
+
+    # def __init__(self):
+    # self.conectar()
+    #
+    # def conectar(self):
+    #     if (self.station.isconnected):
+    #         print("Conectado")
+    #         return
+    #     self.station.active(True)
+    #     self.station.connect(self.ssid, self.password)
+    #     while self.station.isconnected() == False:
+    #         print("Não conectado")
+    #         pass
+
+    def servidor(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 5003))
+        server.listen(1)
+        conn, addr = server.accept()
+        print('Connected by' + str(addr))
+        while True:
+            msg = conn.recv(1024)
+            if (not msg || msg == 'sair'):
+                break
+            print(addr, msg.decode('utf-8'), end='\n')
+        msg = "Sucesso"
+        conn.send(msg.encode('utf-8'))
+        print("Finalizando")
+        conn.close()
