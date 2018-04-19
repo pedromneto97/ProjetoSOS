c1ddcd2821beeb95ad9572195ebdd7d2e44c1fc3
pedromneto97 <pedromneto97@gmail.com>
2018-04-19 19:32:34 -0300

    Correção

--- ESP8266-Client/client.py
+++ ESP8266-Client/client.py
@@ -22,15 +22,18 @@ class Client:
 
     def client(self):
         HOST = '127.0.0.1'  # Endereco IP do Servidor
-        PORT = 5003  # Porta que o Servidor esta
+        PORT = 5000  # Porta que o Servidor esta
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         dest = (HOST, PORT)
         tcp.connect(dest)
         print('Para sair use CTRL+X\n')
         msg = input()
-        while msg != 'sair':
+        while True:
             tcp.send(msg.encode('utf-8'))
+            if msg == 'sair':
+                break
             msg = input()
         msg = tcp.recv(1024)
+
         print(msg.decode('utf-8'))
         tcp.close()
