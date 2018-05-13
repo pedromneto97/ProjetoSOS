d67fdffd35d154144dfb7f188e09037c5443ff84
pedromneto97 <pedromneto97@gmail.com>
2018-05-13 20:13:51 -0300

    Botão para requisitar ajuda

--- ESP32-Client/main.py
+++ ESP32-Client/main.py
@@ -8,32 +8,46 @@ from gc import collect
 collect()
 
 from connect import Connect
-from machine import Pin, ADC, RTC, unique_id, I2C, reset
+from machine import Pin, ADC, RTC, unique_id, I2C, reset, Timer
 from time import sleep
 from client import Client
 from pulseira import Pulseira
 
 class Device:
     def __init__(self):
+
         self.c = Connect()
         self.connection = self.c.start()
         self.client = Client(self.connection)
         self.pulseira = Pulseira()
-        self.botao = Pin(15,Pin.IN,Pin.PULL_DOWN)
-        self.botao.irq(trigger= Pin.IRQ_RISING, handler=self.pressionado)
-        self.saida = Pin(14,Pin.OUT)
-        self.botao.irq(trigger= Pin.IRQ_FALLING, handler=self.liberado)
+
+        self.p4 = Pin(4, Pin.OUT)
+        self.p4.value(1)
+        self.p2 = Pin(2, Pin.OUT)
+        self.p15 = Pin(15, Pin.IN, Pin.PULL_DOWN)
+        self.p15.irq(trigger=Pin.IRQ_RISING, handler=self.button)
+
         id = unique_id() 
         self.rtc = ()
         self.hex_id = b'{:02x}{:02x}{:02x}{:02x}'.format(id[0], id[1], id[2], id[3])
         listHexId = [];
         listHexId.append(self.hex_id)
-    
-    def pressionado(self, p):
-        self.saida.value(1)
-        self.client.client(self.pulseira.config)
-    def liberado(self,p):
-        self.saida.value(0)
+
+    def ativa(self, p):
+        self.p4.value(1)
+        p.deinit()
+
+    def button(self,p):
+        self.desativa_botao()
+        self.p2.value(1)
+        sleep(2)
+        self.p2.value(0)
+        self.client.client(self.pulseira.config, "Ajuda")
+
+    def desativa_botao(self):
+        self.p4.value(0)
+        t = Timer(-1)
+        t.init(period=15000, mode=Timer.ONE_SHOT, callback= self.ativa)
         
     
 def main():
@@ -61,6 +75,5 @@ def main():
                 boot = False 
                 device.rtc = RTC()
                 break
-    device.client.client(device.pulseira.config, 'Ajuda')
 
 main()
