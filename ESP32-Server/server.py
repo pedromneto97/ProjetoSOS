20d0989251a5d4f7d8edd61f5c5714cd045ab176
pedromneto97 <pedromneto97@gmail.com>
2018-06-02 17:16:59 -0300

    Implementação de um watchdog, reinicia o esp caso ocorra um erro

--- ESP32-Server/server.py
+++ ESP32-Server/server.py
@@ -1,6 +1,9 @@
-import network
 import socket
 from gc import collect
+
+from machine import reset
+
+
 # Classe do server
 class Server:
 
@@ -12,37 +15,40 @@ class Server:
     # Servidor
     def servidor(self, lista):
         # Cria o socket
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        confs = self.station.ifconfig()  # Recebe as configurações de endereço
-        aux = confs[0].split('.')  # Sepera o endereço IP por ponto
-        # Monta o 192.168.1.25
-        ip = str(aux[0]) + '.' + str(aux[1]) + '.' + str(aux[2]) + '.' + str(100)
-        # Define as configurações de IP, só alterando o IP do server
-        self.station.ifconfig((ip, confs[1], confs[2], confs[3]))
-        # Seta as variáveis como nada
-        confs = None
-        aux = None
-        server.bind((ip, 5000))  # Da bind no endereço
-        server.listen(28)  # Começa a ouvir
-        print("Endereço: "+ ip)
-        print("Esperando:\n")
-        while True:
-            conn, addr = server.accept()  # Recebe a conecção e o endereço
-            print('Connected by' + str(addr))
-            # Laço das mensagens do cliente
-            msg = conn.recv(1024)  # Recebe a mensagen
-            print(addr, msg.decode('utf-8'), end='\n')
-            tipo = msg.decode('utf-8')
-            msg = conn.recv(1024)  # Recebe a mensagen
-            print(addr, msg.decode('utf-8'), end='\n')
-            pessoa = msg.decode('utf-8')
-            msg = conn.recv(1024)  # Recebe a mensagen
-            print(addr, msg.decode('utf-8'), end='\n')
-            quarto = msg.decode('utf-8')
-            lista = {
-                'tipo': tipo,
-                'nome': pessoa,
-                'quarto': quarto
-            }
-            print("Finalizando")
-            conn.close()  # Fecha a conexão
+        try:
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            confs = self.station.ifconfig()  # Recebe as configurações de endereço
+            aux = confs[0].split('.')  # Sepera o endereço IP por ponto
+            # Monta o 192.168.1.25
+            ip = str(aux[0]) + '.' + str(aux[1]) + '.' + str(aux[2]) + '.' + str(100)
+            # Define as configurações de IP, só alterando o IP do server
+            self.station.ifconfig((ip, confs[1], confs[2], confs[3]))
+            # Seta as variáveis como nada
+            confs = None
+            aux = None
+            server.bind((ip, 5000))  # Da bind no endereço
+            server.listen(28)  # Começa a ouvir
+            print("Endereço: " + ip)
+            print("Esperando:\n")
+            while True:
+                conn, addr = server.accept()  # Recebe a conecção e o endereço
+                print('Connected by' + str(addr))
+                # Laço das mensagens do cliente
+                msg = conn.recv(1024)  # Recebe a mensagen
+                print(addr, msg.decode('utf-8'), end='\n')
+                tipo = msg.decode('utf-8')
+                msg = conn.recv(1024)  # Recebe a mensagen
+                print(addr, msg.decode('utf-8'), end='\n')
+                pessoa = msg.decode('utf-8')
+                msg = conn.recv(1024)  # Recebe a mensagen
+                print(addr, msg.decode('utf-8'), end='\n')
+                quarto = msg.decode('utf-8')
+                lista = {
+                    'tipo': tipo,
+                    'nome': pessoa,
+                    'quarto': quarto
+                }
+                print("Finalizando")
+                conn.close()  # Fecha a conexão
+        except:
+            reset()
