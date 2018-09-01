d3d6247e74ca9b8241da038f841c949548e157ba
pedromneto97 <pedromneto97@gmail.com>
2018-09-01 17:39:42 -0300

    Receber e disponibilizar as informações

--- ESP32-Server/server.py
+++ ESP32-Server/server.py
@@ -36,39 +36,38 @@ class Server:
                 conn, addr = server.accept()  # Recebe a conecção e o endereço
                 print('Conectado por: ' + str(addr))
                 device.p19.value(1)
-                msg = conn.recv(1024)  # Recebe a mensagen
+                msg = conn.recv(2048)  # Recebe a mensagen
                 list = ujson.loads(msg.decode('utf-8'))
-                if list['mac'] not in device.cadastrados:
+                if list['id'] not in device.cadastrados.keys():
                     device.oled.fill(0)
                     device.oled.text('ATENCAO!', 0, 0)
-                    device.oled.fill('CADASTRAR TRANSMISSOR!', 0, 30)
+                    device.oled.text('CADASTRAR!', 0, 30)
                     device.oled.show()
                     conn.close()
                     server.close()
                     return
-                if list['mac'] in device.lista.keys():
-                    if list['tipo'] in device.lista.get(list['mac']).keys():
-                        device.lista.get(list['mac']).get(list['tipo']).update({
-                            "chamadas": device.lista.get(list['mac']).get(list['tipo'])['chamadas'] + list['chamadas']
+                if list['id'] in device.lista.keys():
+                    if list['tipo'] in device.lista.get(list['id']).keys():
+                        device.lista.get(list['id']).get(list['tipo']).update({
+                            "chamadas": device.lista.get(list['id']).get(list['tipo'])['chamadas'] + list['chamadas']
                         })
                     else:
-                        device.lista.get(list['mac']).update({list['tipo']: {
-                            "horas": list['horas'] + ":" + list['minutos'],
+                        device.lista.get(list['id']).update({list['tipo']: {
+                            "horas": str(list['horas']) + ":" + str(list['minutos']),
                             "chamadas": list['chamadas']
                         }})
                 else:
-                    device.lista.update({list['mac']: {list['tipo']: {
-                        "horas": list['horas'] + ":" + list['minutos'],
+                    device.lista.update({list['id']: {list['tipo']: {
+                        "horas": str(list['horas']) + ":" + str(list['minutos']),
                         "chamadas": list['chamadas']
                     }}})
-                print(device.lista)
                 # TODO-me Agrupar e exibir o número de solicitações por tipo
                 device.oled.fill(0)
-                device.oled.text(device.lista[0]['tipo'], 0, 0)
-                device.oled.text("Nome: " + device.cadastrados[list['mac']]['nome'], 0, 20)
-                device.oled.text("Quarto: " + device.cadastrados[list['mac']]['quarto'], 0, 40)
-                device.oled.text("Horario: " + list['horas'] + ':' + list['minutos'], 0, 60)
-                device.oled.text(list['chamadas'], 110, 60)
+                device.oled.text(list['tipo'], 0, 0)
+                device.oled.text("Nome: " + device.cadastrados[list['id']]['nome'], 0, 10)
+                device.oled.text("Quarto: " + str(device.cadastrados[list['id']]['quarto']), 0, 20)
+                device.oled.text("Horario: " + device.lista.get(list['id']).get(list['tipo'])['horas'], 0, 30)
+                device.oled.text(str(device.lista.get(list['id']).get(list['tipo'])['chamadas']), 110, 55)
                 if len(device.lista) > 1:
                     device.oled.text("+", 110, 0)
                 device.oled.show()
